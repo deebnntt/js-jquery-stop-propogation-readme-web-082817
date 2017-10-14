4762882a9dc59a915ebb9f3be4e42634442ab693
deebnntt <daniellebennettchan@gmail.com>
2017-10-14 18:18:34 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	// toggle and delete
+	// $('.course').on('click', function() {
+	// 	$(this)
+	// 		.find('.detail')
+	// 		.slideToggle();
+	// });
+	//
+	$('.course .delete').on('click', function() {
+		alert('about to delete');
+	});
 
-  // toggle and delete
-  $('.course').on('click', function(){
-    $(this).find('.detail').slideToggle();
-  });
+	$('.course').on('click', function() {
+		$(this)
+			.find('.detail')
+			.slideToggle();
+	});
 
-  $('.course .delete').on('click', function(){
-    alert("about to delete");
-  });
-
-  //stop propagation
-  // $('.course').on('click', function(){
-  //   $(this).find('.detail').slideToggle();
-  // });
-
-  // $('.course .delete').on('click', function(event){
-  //   alert("about to delete");
-  // event.stopPropagation();
-
-  // });
+	$('.course .delete').on('click', function(event) {
+		alert('about to delete');
+		event.stopPropagation();
+	});
 });
